50f0860492a0e6c131dff25836f8b76d595b3ebd
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-23 01:10:16 +0600

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Group, ChatInfo
 
 # Create your views here.
+def index(request, group_name):
+    print("Group Name", group_name)
+    group = Group.objects.filter(name = group_name).first()
+    chats = []
+    if group:
+        chats = ChatInfo.objects.filter(group= group)
+
+    else:
+        Group.objects.create(name = group_name)
+    return render(request, 'chat/index.html', {'groupname': group_name, 'chats':chats})
