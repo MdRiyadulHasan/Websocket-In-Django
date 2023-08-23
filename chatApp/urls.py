c57b0fcfacfe3cd208351ad9836c5a3de35d3207
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-23 11:24:16 +0600

    Update urls.py

--- chatApp/urls.py
+++ chatApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chat.urls')),
+    path('home/', include('chat.urls')),
+    path('', include('Practice.urls')),
 
 ]
