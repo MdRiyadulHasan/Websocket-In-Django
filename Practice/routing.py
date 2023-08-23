1c7a88615236636c322b4568e6fe719ae124a910
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-23 17:15:06 +0600

    Update routing.py

--- Practice/routing.py
+++ Practice/routing.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import consumers
 websocket_urlpatterns =[
-    path('ws/mim1/', consumers.MySyncConsumer.as_asgi()),
-    path('ws/mim2/<str:groupname>/', consumers.MyASyncConsumer.as_asgi()),
+    path('ws/mim1/', consumers.MyWebSocketConsumer.as_asgi()),
+    path('ws/mim2/', consumers.MyAsyncWebsocketConsumer.as_asgi()),
+    path('ws/mim3/', consumers.MyJsonWebsocketConsumer.as_asgi()),
+    path('ws/mim4/', consumers.MyAsyncJsonWebsocketConsumer.as_asgi()),
+    # path('ws/mim2/<str:groupname>/', consumers.MyASyncConsumer.as_asgi()),
 ]
